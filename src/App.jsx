84009fe30e910ac9275f68569025bac67ec2879c
eobcre <eobcre@gmail.com>
2023-04-30 13:33:02 -0400

    map loop the list logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 const App = () => {
+  const characters = ['Mario', 'Luigi', 'Yoshi', 'Bowser'];
+
   return (
     <div className='filter-container'>
       <h1>Filter Demo</h1>
       <input type='text' />
+      <ul>
+        {characters.map((character) => (
+          <li key={character}>{character}</li>
+        ))}
+      </ul>
     </div>
   );
 };
